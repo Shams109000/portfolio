7557b7ae5fafedde5b03e6f48d5eeca5040ca019
Shams109000 <shamsalam804@gmail.com>
2024-11-02 21:04:57 +0530

    second commit3

--- src/pages/SkillsSection.jsx
+++ src/pages/SkillsSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Grid, Card, CardContent, Typography } from "@mui/material";
 
-// Update the skills array with image URLs
+
 const skills = [
   { name: "HTML", image: "https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_640.png" },
   { name: "CSS", image: "https://images.javatpoint.com/csspages/images/css-tutorial.png" },
@@ -14,7 +14,6 @@ const skills = [
   { name: "MongoDB", image: "https://camo.githubusercontent.com/e2219317cf5e191a42545b0e3ebff6e0a11887ab6e233fbd6833a43d914af9bc/68747470733a2f2f7777772e646573757669742e636f6d2f77702d636f6e74656e742f75706c6f6164732f323032312f30332f6d6f6e676f64622d69636f6e2e706e67" },
 ];
 
-// Wrap the component with React.memo
 const SkillsSection = React.memo(() => {
   return (
     <section style={{ padding: "2rem 0" }}>
